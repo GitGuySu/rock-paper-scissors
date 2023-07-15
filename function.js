afde7f13732bf01ad0cac323e23303cb1bd016ea
Suhas P Shroff <97375973+GitGuySu@users.noreply.github.com>
2023-07-15 16:40:07 +0530

    Add a basic buttons for the user with result printing on screen. For one round

--- function.js
+++ function.js
@@ -6,40 +6,70 @@ function getComputerChoice () {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
-        alert ("It is a tie !!");
+        return ("It is a tie !!");
     }
     else if (playerSelection == "Rock") {
         if (computerSelection == "Paper") {
-            alert ( "You lose! Paper covers Rock.");
+            return ( "You lose! Paper covers Rock.");
         }
         else {
-            alert ( "Congratulations!! You win with rock beating scissors");
+            return ( "Congratulations!! You win with rock beating scissors");
         }
     }
     else if (playerSelection == "Paper") {
         if (computerSelection == "Scissor") {
-            alert ( "You Lose!!! Scissor cuts paper." );
+            return ( "You Lose!!! Scissor cuts paper." );
         }
         else {
-            alert ( "Congratulations!! you Win with paper covering the rock ");
+            return ( "Congratulations!! you Win with paper covering the rock ");
         }
     }
     else if (playerSelection == "Scissor") {
         if (computerSelection == "Rock") {
-            alert ( "You LOSE!!!! The rock smashes your scissor!");
+            return ( "You LOSE!!!! The rock smashes your scissor!");
         }  
         else {
-            alert ( "Congratulations!! You win with scissors cutting paper");
+            return ( "Congratulations!! You win with scissors cutting paper");
         }
     }
 }
 
-function game() {
-    let player
-    for(let i=0;i<5;i++) {
-        let playerChoice = prompt("Enter your choice");
-        let computerChoice = getComputerChoice();
-        console.log(`Round ${i+1} : User choice is ${playerChoice} and computer choice is ${computerChoice}`)
-        playRound(playerChoice,computerChoice);
-    }
-}
+// function game() {
+//     for(let i=0;i<5;i++) {
+//         let playerChoice = prompt("Enter your choice");
+//         let computerChoice = getComputerChoice();
+//         console.log(`Round ${i+1} : User choice is ${playerChoice} and computer choice is ${computerChoice}`)
+//         playRound(playerChoice,computerChoice);
+//     }
+// }
+
+let container = document.querySelector('#container');
+
+let resultDiv = document.createElement('div');
+resultDiv.style.cssText = 'font-size:x-large;height=50px';
+container.appendChild(resultDiv);
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', (e) => {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'Rock';
+    e.target.style.background = 'blue';
+    resultDiv.textContent = `Computer selected ${computerChoice}.${playRound(playerChoice,computerChoice)}`;   
+})
+
+let paper = document.querySelector('#paper');
+paper.addEventListener('click', (e) => {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'Paper';
+    e.target.style.background = 'blue';
+    resultDiv.textContent = `Computer selected ${computerChoice}.${playRound(playerChoice,computerChoice)}`;    
+})
+
+let scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', (e) => {
+    let computerChoice = getComputerChoice();
+    let playerChoice = 'Scissor';
+    e.target.style.background = 'blue';
+    resultDiv.textContent = `Computer selected ${computerChoice}.${playRound(playerChoice,computerChoice)}`;   
+})
+
